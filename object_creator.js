beb0e15a8bd91788b9c9e047b19332fac9eb30f3
Virel Montes <jovanymontesv@gmail.com>
2020-04-21 12:20:30 -0700

    Added createMyObject

---
+++ object_creator.js
@@ -0,0 +1,10 @@
+function createMyObject(){
+    let myobject = {foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello:function(){ console.log('hello')}
+    }
+    return myobject;
+}
+
+console.log(createMyObject());
